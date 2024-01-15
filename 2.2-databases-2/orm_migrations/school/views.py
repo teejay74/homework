0e28b59977d0d06e92dfaa8d885686ef1a46db36
Артем Макаров <artem.makarov@uptrade.ru>
2024-01-15 16:23:09 +0500

    add homework2.2 orm_migrations

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,8 +5,13 @@ from .models import Student
 
 
 def students_list(request):
+    students = Student.objects.all()
+
+    for student in students:
+        print(student.teachers.all())
+        pass
     template = 'school/students_list.html'
-    context = {}
+    context = {'students': students}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -69,13 +69,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '123',
     }
 }
 
